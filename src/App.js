2f868f39b555f1ceb6a0ad79db2703e3e877547d
Sumin Lee <breath0312@gmail.com>
2024-07-09 22:33:58 +0900

    Home Screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes,Route,Link} from "react-router-dom";
 import Register from "./pages/Register";
 import Splash from "./pages/Splash";
 import Onboarding from "./pages/Onboarding";
+import Home from "./pages/Home";
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/register" element={<Register/>}></Route>
           <Route path="/splash" element={<Splash/>}></Route>
           <Route path="/onboarding" element={<Onboarding/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
       </Routes>
     </>
   );
